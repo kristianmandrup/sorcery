e69e5e99ec787f132be7eb6bf3465c1187821e8c
Kristian Mandrup <kmandrup@gmail.com>
2012-12-03 11:57:45 +0100

    add validators to core concern for mongoid

--- lib/generators/sorcery/templates/mongoid/core.rb
+++ lib/generators/sorcery/templates/mongoid/core.rb
@@ -4,13 +4,17 @@ class <%= model_class_name %>
 
     included do
       include Mongoid::Timestamps
-      # if you use another field as a username, for example email, you can safely remove this field.      
+      # if you use another field as a username, for example email, you can safely remove this field.
       field :username,         :type => String
 
       # if you use this field as a username, you might want to make it :null => false.
       field :email,            :type => String
       field :crypted_password, :type => String
       field :salt,             :type => String      
+
+      validates :password, :presence => true, :on => :create
+      # validates :password, :confirmation => true
+      validates :email,    :presence => true, :uniqueness => true
     end
   end
 end
